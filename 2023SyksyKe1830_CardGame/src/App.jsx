6d60f568a3165b4565b3e19da27c479fb4093aa6
Your Name <you@example.com>
2023-09-06 19:28:53 +0300

    aloitettiin kortti komponenti

---
+++ 2023SyksyKe1830_CardGame/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Card from './components/Card';
+
+export default function App(){
+  return(
+    <>
+      <h1>Hello World!</h1>
+      <Card/>
+    </>
+  );
+}
---
+++ 2023SyksyKe1830_CardGame/src/components/Card.jsx
@@ -0,0 +1,23 @@
+
+export default function Card(){
+    return(
+        <div>
+            <img src = "http://placekitten.com/120/100"/>
+            <ul>
+                <li>
+                    <span>Cuteness</span>
+                    <span>9</span>
+                </li>
+                <li>
+                    <span>Weight</span>
+                    <span>19</span>
+                </li>
+                <li>
+                    <span>Speed</span>
+                    <span>30</span>
+                </li>
+            </ul>
+            
+        </div>
+    );
+}
