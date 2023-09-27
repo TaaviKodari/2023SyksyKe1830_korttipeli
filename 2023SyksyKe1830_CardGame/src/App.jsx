47158c41be738b065615004b9a2f4377c6a31309
Your Name <you@example.com>
2023-09-27 19:28:05 +0300

    korttien luonti

--- 2023SyksyKe1830_CardGame/src/App.jsx
+++ 2023SyksyKe1830_CardGame/src/App.jsx
@@ -1,44 +1,64 @@
 import './App.css'
 import Card from './components/Card';
+import { useState } from 'react';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min);
 
 const playerCard = {
-  image: 'http://placekitten.com/120/100',
-  stats: [{name:'Cuteness', value: 1},
-          {name:'Speed', value: 50},
-          {name:'Weight', value: 100}]
+  image: 'http://placekitten.com/120/100?image=' + getRandomInt(0,15),
+  stats: [{name:'Cuteness', value: getRandomInt(1,999)},
+          {name:'Speed', value: getRandomInt(1,999)},
+          {name:'Weight', value: getRandomInt(1,999)}]
 };
 const opponentCard = {
-  image: 'http://placekitten.com/120/100?image=2',
-  stats: [{name:'Cuteness', value: 10},
-          {name:'Speed', value: 5},
-           {name:'Weight', value: 50}]
+  image: 'http://placekitten.com/120/100?image=' +getRandomInt(0,15),
+  stats: [{name:'Cuteness', value: getRandomInt(1,999)},
+          {name:'Speed', value: getRandomInt(1,999)},
+           {name:'Weight', value: getRandomInt(1,999)}]
 }
+
+const createCard = (index) =>({
+  image: 'http://placekitten.com/120/100?image=' + index,
+  stats: [{name:'Cuteness', value: getRandomInt(1,999)},
+          {name:'Speed', value: getRandomInt(1,999)},
+          {name:'Weight', value: getRandomInt(1,999)}],
+  id: crypto.randomUUID()
+        });
+
+const deck = Array(16).fill(null).map((_,index) => createCard(index))
 export default function App(){
   
+  const [result, setResult] = useState('');
+
   function compareCards(){
     const playerStat = playerCard.stats[0];
     const opponentStat = opponentCard.stats[0];
 
-    let result = '';
-
     if(playerStat.value === opponentStat.value){
-      result = 'draw';
+      setResult('Draw');
     }
     else if(playerStat.value > opponentStat.value){
-      result = 'winner';
+      setResult('Winner');
     }
     else{
-      result = 'loss';
+      setResult('Loss');
     }
-    console.log(result);
+
   }
 
   return(
     <>
       <h1>Hello World!</h1>
-      <Card card ={playerCard}/>
-      <button onClick={compareCards} type="button">Play</button>
-      <Card card ={opponentCard}/>
+      <div className='game'>
+        <Card card ={playerCard}/>
+        
+        <div className='center-area'>
+          <p>{result || 'Press the button'}</p>
+          <button onClick={compareCards} type="button">Play</button>
+        </div>
+
+        <Card card ={opponentCard}/>
+      </div>
     </>
   );
 }
