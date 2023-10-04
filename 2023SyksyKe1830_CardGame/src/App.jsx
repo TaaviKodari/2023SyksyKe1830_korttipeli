cd4b106a34c5640601738e46b6a858ecde8ee628
Your Name <you@example.com>
2023-10-04 19:28:17 +0300

    korttien jako

--- 2023SyksyKe1830_CardGame/src/App.jsx
+++ 2023SyksyKe1830_CardGame/src/App.jsx
@@ -26,13 +26,22 @@ const createCard = (index) =>({
         });
 
 const deck = Array(16).fill(null).map((_,index) => createCard(index))
+const half = Math.ceil(deck.length /2);
+const dealCards = () =>{
+  return{
+    player: deck.slice(0,half),
+    opponent: deck.slice(half)
+  }
+
+}
 export default function App(){
   
   const [result, setResult] = useState('');
+  const[cards, setCards] = useState(dealCards);
 
   function compareCards(){
-    const playerStat = playerCard.stats[0];
-    const opponentStat = opponentCard.stats[0];
+    const playerStat = cards.player[0].stats[0];
+    const opponentStat = cards.opponent[0].stats[0];
 
     if(playerStat.value === opponentStat.value){
       setResult('Draw');
@@ -50,14 +59,16 @@ export default function App(){
     <>
       <h1>Hello World!</h1>
       <div className='game'>
-        <Card card ={playerCard}/>
+        <Card card ={cards.player[0]}/>
         
         <div className='center-area'>
           <p>{result || 'Press the button'}</p>
           <button onClick={compareCards} type="button">Play</button>
         </div>
 
-        <Card card ={opponentCard}/>
+        <Card card ={cards.opponent[0]}/>
+
+        {console.log(dealCards())}
       </div>
     </>
   );
