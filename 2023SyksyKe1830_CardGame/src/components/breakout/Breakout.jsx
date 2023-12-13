1806afcf74ee64844562cccb34cb4dd1d4a5476e
Your Name <you@example.com>
2023-12-13 19:29:15 +0200

    BallMovement kesken

--- 2023SyksyKe1830_CardGame/src/components/breakout/Breakout.jsx
+++ 2023SyksyKe1830_CardGame/src/components/breakout/Breakout.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { BallMovement } from "./BallMovement";
+import data from "./data";
+
+let {ballObj} = data;
 export default function Breakout(){
+    const canvasRef = useRef(null);
+
+    useEffect(()=>{
+        const render = () =>{
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0,0,canvas.width, canvas.height);
+            // ctx.fillStyle = "green";
+            // ctx.fillRect(100,10,150,100);
+            BallMovement(ctx, ballObj);
+            requestAnimationFrame(render);
+        }
+        render();
+    }, []);
+
     return(
-        <canvas id="canvas" height="500px" width="800px"/>
+        <canvas id="canvas" ref={canvasRef} height="500px" width="800px"/>
     );
 }
---
+++ 2023SyksyKe1830_CardGame/src/components/breakout/data.js
@@ -0,0 +1,9 @@
+export default{
+    ballObj:{
+        x: 20,
+        y: 200,
+        dx: 5,
+        dy: 5,
+        rad: 10,
+    }
+}
